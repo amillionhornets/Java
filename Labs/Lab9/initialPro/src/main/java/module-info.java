4c8f72ddb6fbfdc12b2d72226dfe1d5d1456e007
amillionhornets <dylan.j.clark319@gmail.com>
2023-11-01 15:55:13 -0500

    test

--- Labs/Lab9/initialPro/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.initialpro {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.initialpro to javafx.fxml;
-    exports com.example.initialpro;
-}
