2ad91cb13090cce558a14b3dffb0f177a027a380
amillionhornets <dylan.j.clark319@gmail.com>
2023-11-07 14:47:48 -0600

    Started building Calc Project in Javafx
    Project Description:
    This project will be building a Graphical Calculator.

--- Labs/Lab9/initialPro/src/main/java/com/example/demo/HelloApplication.java
+++ Labs/Lab9/initialPro/src/main/java/com/example/demo/HelloApplication.java
@@ -1,37 +1,36 @@
-package com.example.demo;
+// package com.example.demo;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Arc;
-import javafx.scene.paint.Color;
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.layout.Pane;
+// import javafx.stage.Stage;
+// import javafx.scene.shape.Line;
+// import javafx.scene.shape.Arc;
+// import javafx.scene.paint.Color;
 
-import java.io.IOException;
-// Create the letters DC
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        Pane pane = new Pane();
-        Line line = new Line(35,60,35,240); // Line for the D
-        Arc arc = new Arc(35 ,150, 100, 90, -90, 180); // Arc for the D
-        arc.setFill(Color.TRANSPARENT);
-        arc.setStroke(Color.BLACK);
-        Arc arc2 = new Arc(250 ,150, 100, 90, 90, 180); // Arc for the C
-        arc2.setFill(Color.TRANSPARENT);
-        arc2.setStroke(Color.BLACK);
-        pane.getChildren().add(line);
-        pane.getChildren().add(arc);
-        pane.getChildren().add(arc2);
-        Scene scene = new Scene(pane, 300, 300);
-        stage.setTitle("Dyl!");
-        stage.setScene(scene);
-        stage.show();
-    }
+// import java.io.IOException;
+// // Create the letters DC
+// public class HelloApplication extends Application {
+//     @Override
+//     public void start(Stage stage) throws IOException {
+//         Pane pane = new Pane();
+//         Line line = new Line(35,60,35,240); // Line for the D
+//         Arc arc = new Arc(35 ,150, 100, 90, -90, 180); // Arc for the D
+//         arc.setFill(Color.TRANSPARENT);
+//         arc.setStroke(Color.BLACK);
+//         Arc arc2 = new Arc(250 ,150, 100, 90, 90, 180); // Arc for the C
+//         arc2.setFill(Color.TRANSPARENT);
+//         arc2.setStroke(Color.BLACK);
+//         pane.getChildren().add(line);
+//         pane.getChildren().add(arc);
+//         pane.getChildren().add(arc2);
+//         Scene scene = new Scene(pane, 300, 300);
+//         stage.setTitle("Dyl!");
+//         stage.setScene(scene);
+//         stage.show();
+//     }
 
-    public static void main(String[] args) {
-        launch();
-    }
-}
+//     public static void main(String[] args) {
+//         launch();
+//     }
+// }
