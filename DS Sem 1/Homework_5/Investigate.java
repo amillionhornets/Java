dbf8402a9e1da367649ba857c432422ebada2553
amillionhornets <dylan.j.clark319@gmail.com>
2024-10-30 15:05:08 -0500

    Implemented investigate

--- DS Sem 1/Homework_5/Investigate.java
+++ DS Sem 1/Homework_5/Investigate.java
@@ -1,9 +1,18 @@
 public class Investigate {
     public static void main(String [] args) {
         Minimax model = new Minimax (3);
-        System.out.println(model.root);
-        for(Vertex child : model.root.children){
-            System.out.println(child);
+        System.out.println(DFS(model.root, 1));
+        System.out.println(DFS(model.root, 0));
+        System.out.println(DFS(model.root, -1));
+    }
+    public static int DFS(Vertex root, int turn){
+        if(root.children.size() <= 0){
+            return root.utility == turn ? 1 : 0;
         }
+        int returnNum = 0;
+        for(Vertex child : root.children){
+            returnNum += DFS(child, turn);
+        }
+        return returnNum;
     }
 }
