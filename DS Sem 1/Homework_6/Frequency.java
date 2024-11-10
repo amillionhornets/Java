e915236c30160f350f530e94763d08b9092f5a2d
amillionhornets <dylan.j.clark319@gmail.com>
2024-11-09 22:50:34 -0600

    I am to tired for this
    I did problem 1 of hw 6 and I started work on euler 39 again. I need to fix finding the triples. I somehow broke the code and didn't upload the working code to github

--- DS Sem 1/Homework_6/Frequency.java
+++ DS Sem 1/Homework_6/Frequency.java
@@ -1,11 +1,49 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 
 public class Frequency {
     public static void main(String[] args) {
-        String contents = read("franzKafka.txt");
-        System.out.println(soClean(contents));
+        String contents = soClean(read("pg1497.txt"));
+        String[] words = contents.split("\\s+");
+
+        HashMap<String, Integer> frequency = new HashMap<String, Integer>();
+        for (String word : words) {
+            if(frequency.containsKey(word)){
+                frequency.put(word, frequency.get(word) + 1);
+            }else if( word.length() > 5){
+                frequency.put(word, 1);
+            }
+        }
+        String[] keys = frequency.keySet().toArray(new String[0]);
+        int[] values = new int[keys.length];
+        for (int i = 0; i < keys.length; i++) {
+            values[i] = frequency.get(keys[i]);
+        }
+        sort(keys, values, 0, values.length - 1);
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("|%10s | %d |%n", keys[i], values[i]);
+        }
+    }
+    
+    public static void sort(String[] k, int[] v, int lo, int hi){
+        if (lo >= hi){
+            return;
+        }
+        int j = lo, i = lo - 1;
+        do{
+            if(v[j] >= v[hi]){
+                int escrow1 = v[++i]; 
+                String escrow2 = k[i];
+                v[i] = v[j];
+                v[j] = escrow1;
+                k[i] = k[j];
+                k[j] = escrow2;
+            }
+        }while(j++ < hi);
+        sort(k, v, lo, i - 1);
+        sort(k, v, i + 1, hi);
     }
 
     public static String soClean(String bookContent){
--- DS Sem 1/Homework_7/Clark39.py
+++ DS Sem 1/Homework_7/Clark39.py
@@ -4,16 +4,16 @@ import math
 def main():
     triangles = 0
     maxP = 0
-    for p in range(1, 1000):
-        for a in range(1, 1000):           
-            for b in range(a, 1000):      
-                cSqrd = math.pow(a,2) + math.pow(b,2)
-                c = math.floor(math.sqrt(cSqrd))
-                if(math.pow(c,2) == cSqrd and (a + b + c) <= 1000): # check to see if the triangle is an integral right triangle
-                    triangles+=1
-                    if(a+b+c > maxP):
-                        maxP = a + b + c
-    print(maxP)
-
+    p = 120
+    for a in range(1, 1000): 
+        triangles = 0          
+        for b in range(1, 1000):      
+            cSqrd = math.pow(a,2) + math.pow(b,2)
+            c = math.floor(math.sqrt(cSqrd))
+            peri = a + b + c
+            if(c**2 == cSqrd): # check to see if the triangle is an integral right triangle
+                triangles = triangles + 1
+    print(triangles)
+4
 if __name__ == "__main__":
     main()
