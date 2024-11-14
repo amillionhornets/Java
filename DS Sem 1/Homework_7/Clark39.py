96625bff89fb1f1b7fd767fb4b268a1634e19029
amillionhornets <dylan.j.clark319@gmail.com>
2024-11-14 14:00:19 -0600

    finished problem 9 and 39

--- DS Sem 1/Homework_7/Clark39.py
+++ DS Sem 1/Homework_7/Clark39.py
@@ -2,18 +2,18 @@
 import math
 
 def main():
-    triangles = 0
     maxP = 0
-    p = 120
-    for a in range(1, 1000): 
-        triangles = 0          
-        for b in range(1, 1000):      
-            cSqrd = math.pow(a,2) + math.pow(b,2)
-            c = math.floor(math.sqrt(cSqrd))
-            peri = a + b + c
-            if(c**2 == cSqrd): # check to see if the triangle is an integral right triangle
-                triangles = triangles + 1
-    print(triangles)
-4
+    maxTriangles = 0
+    trianglesCount = [0] * 1001
+
+    for a in range(1,1000):
+        triangles = 0
+        for b in range(a, 1000 - a):
+            c = math.sqrt((a**2) + (b**2))
+            p = a+b+int(c)
+            if(c.is_integer() and p <= 1000):
+                trianglesCount[p] += 1
+    maxP = trianglesCount.index(max(trianglesCount))
+    print(maxP)
 if __name__ == "__main__":
     main()
